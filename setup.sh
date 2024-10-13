e7336f6eed507b8d9e65e36ab0bdc9000f6b7591
testtest132 <tankun.p@aware.co.th>
2024-10-13 22:30:30 +0700

    add table

--- setup.sh
+++ setup.sh
@@ -2,11 +2,18 @@
 
 export PGPASSWORD='Srg9PUT8gGiLU6365ZHlMyhnzlxZoOEE'
 
-pg_dump -h dpg-crucbkjtq21c738gkic0-a.singapore-postgres.render.com \
-    -U mydb_851u_user \
-    -d mydb_851u \
-    --table=users \
-    --data-only \
-    -f /dump/users_data.sql
+tables=("users" "user_roles" "role" "products")
 
-psql -h localhost -U testuser -d test_name -f /dump/users_data.sql
+for table in "${tables[@]}"; do
+    pg_dump -h dpg-crucbkjtq21c738gkic0-a.singapore-postgres.render.com \
+        -U mydb_851u_user \
+        -d mydb_851u \
+        --table="$table" \
+        --data-only \
+        -f "/dump/${table}_data.sql"
+    
+done
+
+for table in "${tables[@]}"; do
+    psql -h localhost -U testuser -d test_name -f "/dump/${table}_data.sql"
+done
